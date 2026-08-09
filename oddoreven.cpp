5c5aa61617de2f53a8e99d21b3ce8188812343c9
minhas <juttminhas82@gmail.com>
2026-08-09 13:30:04 +0500

    upgrade and adding the postive or negative

--- oddoreven.cpp
+++ oddoreven.cpp
@@ -6,7 +6,8 @@ int main(){
     cout<<"enter the no : ";
     cin>>num;
     if(num!=0){
- cout<<(num%2==0 ? "the num is even ":"the num is odd");
+ cout<<(num%2==0 ? "the num is even ":"the num is odd ");
+ cout<<(num>0 ?"and no is postive ":"and no is negative ");
     }
     else
     cout<<"enter the other num 0 is not even or odd";
