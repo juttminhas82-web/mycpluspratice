83e0ffe34cce65296cf3725377bd5a38b9d86bb0
minhas <juttminhas82@gmail.com>
2026-08-09 12:50:51 +0500

    ternary condition

---
+++ ternaryconditon.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"enter the age : ";
+    cin>>age;
+    cout<<(age>=18? "you are big boy " : "still times just make mistake ");
+    return 0;
+}
