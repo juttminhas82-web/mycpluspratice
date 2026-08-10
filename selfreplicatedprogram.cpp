d664426b414b4513b5bb2bda9d88307f4a256bd0
minhas <juttminhas82@gmail.com>
2026-08-10 18:37:30 +0500

    add the quine concept program

---
+++ selfreplicatedprogram.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    const char* code=" #include<iostream>\nusing namespace std;\nint main(){\ncout<<\"hello\";\nreturn 0;\n}";
+   cout<<code,code;
+    return 0;
+}
